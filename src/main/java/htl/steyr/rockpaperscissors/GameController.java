cec363a37efe2b2cca875e8c7475e88110ff4fd2
LeBonsBay <armin.keric0001@gmail.com>
2025-12-01 16:34:50 +0100

    small changes...

--- src/main/java/htl/steyr/rockpaperscissors/GameController.java
+++ src/main/java/htl/steyr/rockpaperscissors/GameController.java
@@ -5,7 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,7 +25,30 @@ public class GameController implements Initializable {
     private GameLogic gameLogic = new GameLogic(null);
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        backGroundMusic();
+    }
+
+    public void backGroundMusic() {
+        //@ToDo
+        //implement button for stopping music and implement infinite-loop (as long as the program runs)
+        //sometimes the music stops while playing, idk yet why
+        //Note: TinySound library seems to be good
+
+
+        String mp3File = "./src/main/resources/mp3/bgMusic.mp3";
+        Media bgMusic = new Media(new File(mp3File).toURI().toString());
+
+        MediaPlayer mediaPlayer = new MediaPlayer(bgMusic);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+
+        mediaPlayer.setVolume(0.1);
+
+        mediaPlayer.play();
+
+
+    }
 
 
     public void onClick(ActionEvent actionEvent) {
